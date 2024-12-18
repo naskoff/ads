a7b2f598a468b303cc35c3129d572a8deab58335
Zlatin Hristov <zlatin.hristov@softavis.com>
2024-12-18 16:05:19 +0200

    Add status to ad template form

--- app/Enums/AdTemplateStatus.php
+++ app/Enums/AdTemplateStatus.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-enum AdTemplateStatus: string
+use Filament\Support\Contracts\HasLabel;
+
+enum AdTemplateStatus: string implements HasLabel
 {
     case Draft = 'draft';
     case InProgress = 'in-progress';
     case Completed = 'completed';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Draft => 'Draft',
+            self::InProgress => 'In Progress',
+            self::Completed => 'Completed',
+        };
+    }
 }
--- app/Filament/Resources/AdTemplateResource/RelationManagers/TemplateRelationManager.php
+++ app/Filament/Resources/AdTemplateResource/RelationManagers/TemplateRelationManager.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\AdTemplateResource\RelationManagers;
 
+use App\Enums\AdTemplateStatus;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
@@ -18,14 +19,18 @@ class TemplateRelationManager extends RelationManager
     {
         return $form
             ->schema([
+                Forms\Components\Select::make('status')
+                    ->required()
+                    ->options(AdTemplateStatus::class),
                 Forms\Components\TextInput::make('title')
+                    ->columnSpan('full')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->required()
+                Forms\Components\Textarea::make('description')
+                    ->columnSpan('full')
                     ->maxLength(255),
                 Forms\Components\TextInput::make('canva_url')
-                    ->required()
+                    ->rules('url')
                     ->maxLength(255),
             ]);
     }
@@ -36,12 +41,25 @@ class TemplateRelationManager extends RelationManager
             ->recordTitleAttribute('title')
             ->paginated(false)
             ->columns([
-                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('canva_url')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->headerActions([
                 Tables\Actions\CreateAction::make()
                     ->hidden($this->getOwnerRecord()->template()->exists())
-                    ->createAnother(false)
+                    ->after(fn () => $this->getOwnerRecord()->updateStatus())
+                    ->createAnother(false),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
